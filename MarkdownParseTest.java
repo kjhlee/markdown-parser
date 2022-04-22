a4a0c847a2c6f9cbafe192c6b805f1ca02e4cefa
theemotionalpie <52635340+theemotionalpie@users.noreply.github.com>
2022-04-22 08:42:09 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,8 @@
 import static org.junit.Assert.*;
-
+import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import org.junit.*;
 import java.util.ArrayList;
 public class MarkdownParseTest {
 
